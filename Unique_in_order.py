20b8c67b4556e08413be2d9ab5a4d91c482a396c
Aakriti <55615182+aakriti-aggarwal-11@users.noreply.github.com>
2020-05-11 20:00:00 -0600

    Add files via upload

---
+++ Unique_in_order.py
@@ -0,0 +1,12 @@
+def unique_in_order(iterable):
+    lame = list(iterable)
+    a = len(iterable)
+    arr = []
+    if a > 0:
+        arr = [lame[0]]
+        for i in range (0, a - 1):
+            if lame[i] != lame[i + 1]:
+                arr.append(lame[i + 1])
+    return arr
+#codewars
+    
