a851b9800296e2b3a16e453dcc0e20e0adca2b31
Aakriti Aggarwal <aggarwal.aa.01@gmail.com>
2020-06-02 19:08:29 +0530

    update repo

---
+++ Is_Prime.py
@@ -0,0 +1,12 @@
+def is_prime(num):
+    if num < 1:
+        return False
+    elif num == 2:
+        return True
+    elif num == 1:
+        return False
+    else:
+        for i in range(2, int(num/2)+1):
+            if num % i == 0:
+                return False
+        return True
